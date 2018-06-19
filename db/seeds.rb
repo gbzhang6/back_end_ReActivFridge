7791380ee2b1db392f567802ac173bc4d976abb2
myungbeans <michael.h.myung@gmail.com>
2018-06-19 17:54:11 -0400

    Connected Seed to API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,24 @@
 require 'byebug'
 require 'rest-client'
+require 'json'
 
 Ingredient.delete_all
 UserIngredient.delete_all
-
+User.delete_all
 
 User.create(name: "GBZ", email: "gbz@gmail.com", password: "gbz123")
+#temporarily make Gui the only user
 
-ingredients = ['lime', 'apple', 'tortilla', 'pork', 'onion', 'pepper', 'salt', 'avocado', 'pasta', 'garlic', 'cheese']
-ingredients.each {|item| 
-    Ingredient.create(name: item, imgUrl:"https://www.themealdb.com/images/ingredients/#{item}.png")
+def imgUrl(imgObj)
+    name = imgObj["strIngredient"].gsub(" ", "%20")
+    return "https://www.themealdb.com/images/ingredients/#{name}.png"
+end
+
+res = RestClient.get 'https://www.themealdb.com/api/json/v1/1/list.php?i=list', {accept: :json}
+data = JSON.parse(res)
+data["meals"].each {|item|
+    Ingredient.create(name: item["strIngredient"], imgUrl: imgUrl(item))
 }
 
-# RestClient.get()
-
-Ingredient.all.each {|i| UserIngredient.create(user_id:1, ingredient_id: i.id)}
+Ingredient.all.each {|i| UserIngredient.create(user_id: 1, ingredient_id: i.id)}
+#temporarily assign all ingredients to Gui
