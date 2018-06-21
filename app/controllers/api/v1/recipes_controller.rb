b55bb6b644ab90ce40447e595f1745e2f4287982
myungbeans <michael.h.myung@gmail.com>
2018-06-21 18:59:30 -0400

    testing api connection with front

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,6 +2,7 @@ require 'byebug'
 
 class Api::V1::RecipesController < ApplicationController
   def results
+    console.log("Cooking up the results of your query...")
     q = params[:q]
     @recipes = makeFetchHappen(q)
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,25 @@ require 'rest-client'
 require 'json'
 require 'net/http'
 require 'open-uri'
+require 'byebug'
 
 class ApplicationController < ActionController::API
     def makeFetchHappen(query)
         
         yummlyURL = "http://api.yummly.com/v1/api/recipes?_app_id=#{ENV['API_ID']}&_app_key=#{ENV['API_KEY']}&requirePictures=true&q=#{query}"
-        q = ""
-        query.split('%20').each{|item| q = q+`&allowedIngredient[]=#{item}`}
-        finalURL = yummlyURL + q
+        que = ""
+        query.split('%20').each{|item| que = que+"&allowedIngredient[]=#{item}"}
+        finalURL = yummlyURL + que
+
+        # debugger
 
         link = URI.parse(finalURL)
         response = Net::HTTP.get_response(link)
         response.body
         result = JSON.parse(response.body)
+        # debugger
+        # result["matches"].sort_by{|h| h["rating"]}.reverse
+        
         return result
     end
 end
